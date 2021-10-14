e0d074c26dba10fe773ddad9c2ee006143798653
Tomas Baca <klaxalk@gmail.com>
2021-10-14 12:47:39 +0200

    updated install.sh

--- install/install_intel.sh
+++ install/install_intel.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
 sudo apt-get -y install ocl-icd-dev ocl-icd-libopencl1 ocl-icd-opencl-dev opencl-headers
 
-mkdir -p ~/neo
-cd ~/neo
+mkdir -p /tmp/neo
+cd /tmp/neo
 wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-gmmlib_20.1.1_amd64.deb
 wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-core_1.0.3826_amd64.deb
 wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-opencl_1.0.3826_amd64.deb
