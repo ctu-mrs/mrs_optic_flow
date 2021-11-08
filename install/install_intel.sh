6ed39dea061f4f0819055434e2bdd6c44d6627c2
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-11-08 14:18:55 +0100

    install NEO OpenCL drivers only for Intel GPUs

--- install/install_intel.sh
+++ install/install_intel.sh
@@ -7,14 +7,19 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 sudo apt-get -y install ocl-icd-dev ocl-icd-libopencl1 ocl-icd-opencl-dev opencl-headers
 
-mkdir -p /tmp/neo
-cd /tmp/neo
-wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-gmmlib_20.1.1_amd64.deb
-wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-core_1.0.3826_amd64.deb
-wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-opencl_1.0.3826_amd64.deb
-wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-opencl_20.16.16582_amd64.deb
-wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-ocloc_20.16.16582_amd64.deb
-sudo dpkg -i *.deb
+vendor=$(lscpu | awk '/Vendor ID/{print $3}')
+if [[ "$vendor" == "GenuineIntel" ]]; then
+  mkdir -p /tmp/neo
+  cd /tmp/neo
+  wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-gmmlib_20.1.1_amd64.deb
+  wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-core_1.0.3826_amd64.deb
+  wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-igc-opencl_1.0.3826_amd64.deb
+  wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-opencl_20.16.16582_amd64.deb
+  wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-ocloc_20.16.16582_amd64.deb
+  sudo dpkg -i *.deb
+else
+    echo "Not installing NEO OpenCL drivers for non-Intel CPU vendor, which is \"$vendor\"."
+fi
 
 if [ -e /etc/lightdm ]; then
   sudo sh -c "echo [SeatDefaults]'\n'autologin-user=$USER'\n'autologin-user-timeout=0>/etc/lightdm/lightdm.conf"
