14a95868367bb9300f4e3e2364d6756887d97b78
Tomas Baca <klaxalk@gmail.com>
2021-11-10 19:18:22 +0100

    fixed install.sh to not enable autologin by default

--- install/install_intel.sh
+++ install/install_intel.sh
@@ -5,6 +5,17 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
+unattended=0
+for param in "$@"
+do
+  echo $param
+  if [[ $param == "--unattended" ]]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
+
 sudo apt-get -y install ocl-icd-dev ocl-icd-libopencl1 ocl-icd-opencl-dev opencl-headers
 
 vendor=$(lscpu | awk '/Vendor ID/{print $3}')
@@ -18,9 +29,33 @@ if [[ "$vendor" == "GenuineIntel" ]]; then
   wget https://github.com/intel/compute-runtime/releases/download/20.16.16582/intel-ocloc_20.16.16582_amd64.deb
   sudo dpkg -i *.deb
 else
-    echo "Not installing NEO OpenCL drivers for non-Intel CPU vendor, which is \"$vendor\"."
+  echo "Not installing NEO OpenCL drivers for non-Intel CPU vendor, which is \"$vendor\"."
 fi
 
-if [ -e /etc/lightdm ]; then
-  sudo sh -c "echo [SeatDefaults]'\n'autologin-user=$USER'\n'autologin-user-timeout=0>/etc/lightdm/lightdm.conf"
-fi
+default=n
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=y
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mEnable autologin in lightdm? Should be enabled only for real UAVs!!! [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
+
+    if [ -e /etc/lightdm ]; then
+      sudo sh -c "echo [SeatDefaults]'\n'autologin-user=$USER'\n'autologin-user-timeout=0>/etc/lightdm/lightdm.conf"
+    fi
+
+    break
+
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+done
